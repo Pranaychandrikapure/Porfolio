74e814a9e07b4649e2964890e43fdeab790f4b3b
Pranay Chandrikapure <116444922+Pranaychandrikapure@users.noreply.github.com>
2023-06-30 10:26:05 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".auto-type",{
+    strings:["Frontend Developer","UI Designer","Backend Developer"],
+    typeDelay:2,
+    typeSpeed:130,
+    backSpeed:110,
+    loop:true
+})
